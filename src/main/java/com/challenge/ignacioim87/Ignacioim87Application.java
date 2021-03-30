61e437fa6523fbd8ca3a379e09912433a3da6faf
ignacioim87 <ignaciom8719@gmail.com>
2021-03-30 18:20:27 -0300

    adding spring files

---
+++ src/main/java/com/challenge/ignacioim87/Ignacioim87Application.java
@@ -0,0 +1,13 @@
+package com.challenge.ignacioim87;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ignacioim87Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ignacioim87Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/ignacioim87/Ignacioim87ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ignacioim87;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ignacioim87ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
